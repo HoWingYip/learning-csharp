be90be5c7b234bdbde5a2f8f8d0120b2120f298d
HoWingYip <sploooosh884@gmail.com>
2018-09-06 21:29:00 +0800

    Completed Assignment 2a!

---
+++ IFAssignments/1a/1a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText
+            (@"C:\Users\yip\learning-csharp\IFAssignments\1a\data.txt");
+            //read file
+
+            string[] numbers = text.Split(' ');
+            string[] outputNumbers = text.Split(' ');
+            //split string when spaces occur
+            foreach (string number in numbers)
+            {
+                string outputNumber = number + "\r\n";
+                Console.Write(outputNumber);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IFAssignments/2a/2a/2a/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, sum = 0, score = 0, highest = Int32.MinValue, lowest = Int32.MaxValue, convertedNumber = 0;
+
+            string[] numbers = System.IO.File.ReadAllText
+            (@"C:\Users\yip\learning-csharp\IFAssignments\2a\data2.txt").Split(' ');
+            //read file
+
+            foreach (string number in numbers)
+            {
+                n++;
+                convertedNumber = Int32.Parse(number);
+                sum = sum + convertedNumber;
+                if (convertedNumber > highest) highest = convertedNumber;
+                if (convertedNumber < lowest) lowest = convertedNumber;
+            }
+
+            Console.Write("Number of numbers: "); Console.Write(n); Console.Write("\n");
+            Console.Write("Sum of numbers: "); Console.Write(sum); Console.Write("\n");
+            Console.Write("Highest: "); Console.Write(highest); Console.Write("\n");
+            Console.Write("Lowest: "); Console.Write(lowest); Console.Write("\n");
+            Console.ReadKey();
+        }
+    }
+}
